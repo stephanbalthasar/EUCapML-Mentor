8ce70649700f160042654ec3af112868259ba862
Stephan Balthasar <stephan.balthasar@uni-bayreuth.de>
2026-03-11 17:29:45 +0100

    Delete deadcode directory

--- deadcode/parse.py
+++
@@ -1,7 +0,0 @@
-def parse_booklet(docx_path: str):
-    """
-    Returns:
-        chunks: List[str]
-        metas:  List[dict] (e.g., {'paras': [...], 'cases': [...], 'page_num': int})
-    """
-    raise NotImplementedError
